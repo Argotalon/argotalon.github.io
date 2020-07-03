fc8dd148dcd442e35834fb5589614cd86976ecdc
Argotalon <nicholasdownie@gmail.com>
2020-07-02 20:34:13 -0400

    Revamped Amortization Schedule

--- amortize.js
+++ amortize.js
@@ -38,12 +38,20 @@ function calculate() {
     totalInt = eval(inPayRaw.join('+'));
     totalPr = eval(prPayRaw.join('+'));
     console.log(totalInt);
+
+    if(i % 12 == 0){
+      document.getElementById('schedule').innerHTML += "<tr><td colspan='4' class='tHeader'><h2>Year " + (i/12+1) +"</h2></td></tr>";
+      document.getElementById('schedule').innerHTML += "<tr><th>Month</th><th>Interest Payment</th><th>Prinicipal Payment</th><th>Prinicipal Balance</th></tr>"
+    }
+
     i++;
-    document.getElementById('schedule').innerHTML += "<p class='schedule'>" + "Month " + i + ": Interest Payment: " + inPayDisplay + " Principal Payment: "+ prPayDisplay + " Prinicipal Balance: " + principalDisplay + '</p>' ;
+
+    document.getElementById('schedule').innerHTML += "<tr><td>Month: " + i + "</td><td>" + inPayDisplay + "</td><td>" + prPayDisplay + "</td><td>" + principalDisplay + "</td></tr>";
+    /*"<p class='schedule'>" + "Month " + i + ": Interest Payment: " + inPayDisplay + " Principal Payment: "+ prPayDisplay + " Prinicipal Balance: " + principalDisplay + '</p>';*/
     if (i == period) {
-      document.getElementById('intPaid').innerHTML += totalInt.toFixed(2);
+      document.getElementById('intPaid').innerHTML = totalInt.toFixed(2);
       totalPaid = totalPr + totalInt;
-      document.getElementById('totalPaid').innerHTML += totalPaid.toFixed(2);
+      document.getElementById('totalPaid').innerHTML = totalPaid.toFixed(2);
     }
   }
 }
