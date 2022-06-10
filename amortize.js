37b47540b3e6ad63ba6ad04794bca98d3a99821e
Argotalon <66919671+Argotalon@users.noreply.github.com>
2022-06-09 20:04:20 -0400

    Update amortize.js

--- amortize.js
+++ amortize.js
@@ -28,37 +28,38 @@ function calculate() {
   if (periodyr > 30 || periodyr <= 0) {
     throw new Error("Period must be greater than 0 but no higher than 30");
   }
-  
-  discount = (((1 + rate) ** period) - 1) / (rate * (1 + rate) ** period);
-  var p = principalRaw / discount;
-  payment = p.toFixed(2);
+  Else {  
+    discount = (((1 + rate) ** period) - 1) / (rate * (1 + rate) ** period);
+    var p = principalRaw / discount;
+    payment = p.toFixed(2);
 
-  document.getElementById('payment').innerHTML = payment;
+    document.getElementById('payment').innerHTML = payment;
 
-  while (principalRaw > 0 && i < period) {
-    inPayRaw[i] = rate * principalRaw;
-    inPayDisplay = inPayRaw[i].toFixed(2);
-    prPayRaw[i] = p - inPayRaw[i];
-    prPayDisplay = prPayRaw[i].toFixed(2);
-    principalRaw = principalRaw - prPayRaw[i];
-    principalDisplay = principalRaw.toFixed(2);
-    totalInt = eval(inPayRaw.join('+'));
-    totalPr = eval(prPayRaw.join('+'));
-    console.log(totalInt);
+    while (principalRaw > 0 && i < period) {
+      inPayRaw[i] = rate * principalRaw;
+      inPayDisplay = inPayRaw[i].toFixed(2);
+      prPayRaw[i] = p - inPayRaw[i];
+      prPayDisplay = prPayRaw[i].toFixed(2);
+      principalRaw = principalRaw - prPayRaw[i];
+      principalDisplay = principalRaw.toFixed(2);
+      totalInt = eval(inPayRaw.join('+'));
+      totalPr = eval(prPayRaw.join('+'));
+      console.log(totalInt);
 
-    if (i % 12 == 0){
-      document.getElementById('schedule').innerHTML += "<tr><td colspan='4' class='tHeader'><h2>Year " + (i/12+1) +"</h2></td></tr>";
-      document.getElementById('schedule').innerHTML += "<tr><th>Month</th><th>Interest Payment</th><th>Prinicipal Payment</th><th>Prinicipal Balance</th></tr>"
-    }
+      if (i % 12 == 0){
+        document.getElementById('schedule').innerHTML += "<tr><td colspan='4' class='tHeader'><h2>Year " + (i/12+1) +"</h2></td></tr>";
+        document.getElementById('schedule').innerHTML += "<tr><th>Month</th><th>Interest Payment</th><th>Prinicipal Payment</th><th>Prinicipal Balance</th></tr>"
+      }
 
-    i++;
+      i++;
 
-    document.getElementById('schedule').innerHTML += "<tr><td>Month: " + i + "</td><td>" + inPayDisplay + "</td><td>" + prPayDisplay + "</td><td>" + principalDisplay + "</td></tr>";
-    /*"<p class='schedule'>" + "Month " + i + ": Interest Payment: " + inPayDisplay + " Principal Payment: "+ prPayDisplay + " Prinicipal Balance: " + principalDisplay + '</p>';*/
-    if (i == period) {
-      document.getElementById('intPaid').innerHTML = totalInt.toFixed(2);
-      totalPaid = totalPr + totalInt;
-      document.getElementById('totalPaid').innerHTML = totalPaid.toFixed(2);
+      document.getElementById('schedule').innerHTML += "<tr><td>Month: " + i + "</td><td>" + inPayDisplay + "</td><td>" + prPayDisplay + "</td><td>" + principalDisplay + "</td></tr>";
+      /*"<p class='schedule'>" + "Month " + i + ": Interest Payment: " + inPayDisplay + " Principal Payment: "+ prPayDisplay + " Prinicipal Balance: " + principalDisplay + '</p>';*/
+      if (i == period) {
+        document.getElementById('intPaid').innerHTML = totalInt.toFixed(2);
+        totalPaid = totalPr + totalInt;
+        document.getElementById('totalPaid').innerHTML = totalPaid.toFixed(2);
+      }
     }
   }
 }
